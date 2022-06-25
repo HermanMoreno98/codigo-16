8ba8a4de40e33bb531cd50e14a1c962569e1a212
HermanMoreno98 <hermanmoreno020598@gmail.com>
2022-06-25 17:31:42 -0500

    ClaseBucles

--- semana4/js/dia3-2.js
+++ semana4/js/dia3-2.js
@@ -5,3 +5,15 @@ while (contador < 10) {
   contador += 1;
 }
 console.log("Terminó la iteración");
+
+let ar = [];
+let n = 5;
+let potencia = 9;
+let suma = 0;
+for (let i = 0; i < n; i++) {
+  ar.push(Math.pow(10, potencia) + i + 1);
+}
+for (let j = 0; j < ar.length; j++) {
+  suma += ar[j];
+}
+console.log(suma);
