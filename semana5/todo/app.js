cfca8c1c7540f3e082ced03cd19dc232dc4384ea
HermanMoreno98 <hermanmoreno020598@gmail.com>
2022-07-02 14:36:07 -0500

    parte2

--- semana5/todo/app.js
+++ semana5/todo/app.js
@@ -1,78 +1,83 @@
 // DOM:
 const listTasks = document.querySelector("#list");
 const inputTasks = document.querySelector("#input_newtask");
-inputTasks.focus()
+inputTasks.focus();
 
 const arrayTasks = [];
 
-function addTask(){
-    if(inputTasks.value == ""){
-        alert("Ingresa un texto para la tarea");
-        return;
-    }
-    const task = new Task(inputTasks.value);
-    arrayTasks.push(task);
-    document.querySelector(".form").before(task.createElement());
-    inputTasks.value = "";
-    inputTasks.focus();
+function addTask() {
+  if (inputTasks.value == "") {
+    alert("Ingresa un texto para la tarea");
+    return;
+  }
+  const task = new Task(inputTasks.value);
+  arrayTasks.push(task);
+  document.querySelector(".form").before(task.createElement());
+  inputTasks.value = "";
+  inputTasks.focus();
 }
 
 function checkTask(checkbox) {
-    const task_id = checkbox.parentElement.id;
-    const task = arrayTasks.find(task => task.id == task_id);
-    if (checkbox.checked) {
-        task.done();
-        //checkbox.setAttribute('disabled',true);
-    } else {
-        task.removeDone();
-    }
+  if (checkbox.checked) {
+    parentTask(checkbox).done();
+    //checkbox.setAttribute('disabled',true);
+  } else {
+    parentTask(checkbox).removeDone();
+  }
 }
 
-function deleteTask(anchor){
-    const task_id = anchor.parentElement.id;
-    const task = arrayTasks.find(task => task.id == task_id);
-    task.delete();
+function deleteTask(anchor) {
+  parentTask(anchor).delete();
+}
+
+function updateTask(anchor) {
+    const newText = prompt("Ingrese el nuevo nombre de su tarea");
+    parentTask(anchor).update(newText);
+}
+
+function parentTask(element) {
+  return arrayTasks.find(
+    (task) => task.id === element.parentElement.parentElement.id
+  );
 }
 
 const chxTaskDone = document.querySelector("#chx_task_done");
 
 chxTaskDone.onchange = function () {
-    const taskTodo = document.querySelectorAll(".todo"); // Retorna un NodeList, es decir, una lista de elementos
-    const taskDelete = document.querySelectorAll(".delete");
-    if(this.checked) {
-        // Si es TRUE, vamos a mostrar solo las tareas terminadas, por ende vamos a buscar las tareas que tengan la clase 'todo' y 'delete' y las vamos a ocultar
-        taskTodo.forEach((todo) => (todo.style.display = "none"));
-        taskDelete.forEach((task) => (task.style.display = "none"));
-    } else {
-        taskTodo.forEach((todo) => (todo.style.display = "block"));
-        taskDelete.forEach((task) => (task.style.display = "block"));
-    }
-}
+  showOrHideElements(this.checked, ".todo", ".delete");
+  // const taskTodo = document.querySelectorAll(".todo"); // Retorna un NodeList, es decir, una lista de elementos
+  // const taskDelete = document.querySelectorAll(".delete");
+  // if(this.checked) {
+  //     // Si es TRUE, vamos a mostrar solo las tareas terminadas, por ende vamos a buscar las tareas que tengan la clase 'todo' y 'delete' y las vamos a ocultar
+  //     taskTodo.forEach((todo) => (todo.style.display = "none"));
+  //     taskDelete.forEach((task) => (task.style.display = "none"));
+  // } else {
+  //     taskTodo.forEach((todo) => (todo.style.display = "block"));
+  //     taskDelete.forEach((task) => (task.style.display = "block"));
+  // }
+};
 
 const chxTaskDelete = document.querySelector("#chx_task_delete");
 
-chxTaskDelete.onchange = function() {
-    const taskDone = document.querySelectorAll(".done"); 
-    const taskTodo = document.querySelectorAll(".todo");
-    if ( this.checked){
-        taskDone.forEach((done) => (done.style.display = "none"))
-        taskTodo.forEach((todo) => (todo.style.display = "none"))
-    } else {
-        taskDone.forEach((done) => (done.style.display = "block"))
-        taskTodo.forEach((todo) => (todo.style.display = "block"))
-    }
-}
+chxTaskDelete.onchange = function () {
+  showOrHideElements(this.checked, ".done", ".todo");
+};
 
 const chxTaskTodo = document.querySelector("#chx_task_todo");
 
 chxTaskTodo.onchange = function () {
-    const taskDone = document.querySelectorAll(".done");
-    const taskDelete = document.querySelectorAll(".delete");
-    if(this.checked){
-        taskDone.forEach((done) => (done.style.display = "none"));
-        taskDelete.forEach((task) => (task.style.display = "none"));
-    } else {
-        taskDone.forEach((done) => (done.style.display = "block"));
-        taskDelete.forEach((task) => (task.style.display = "block"));
-    }
+  showOrHideElements(this.checked, ".done", ".delete");
+};
+
+function showOrHideElements(checked, typeOne, typeTwo) {
+  const elementsOne = document.querySelectorAll(typeOne);
+  const elementsTwo = document.querySelectorAll(typeTwo);
+
+  if (checked) {
+    elementsOne.forEach((done) => (done.style.display = "none"));
+    elementsTwo.forEach((todo) => (todo.style.display = "none"));
+  } else {
+    elementsOne.forEach((done) => (done.style.display = "block"));
+    elementsTwo.forEach((todo) => (todo.style.display = "block"));
+  }
 }
--- semana5/todo/task.js
+++ semana5/todo/task.js
@@ -1,47 +1,56 @@
 class Task {
+  constructor(_text) {
+    this.id = "_" + Math.random().toString(36).slice(2);
+    this.text = _text;
+    this.status = "todo";
+    this.created_at = new Date();
+    this.done_at = null;
+    this.deleted_at = null;
+  }
 
-    constructor(_text){
-        this.id = '_' + Math.random().toString(36).slice(2);
-        this.text = _text;
-        this.status = 'todo';
-        this.created_at = new Date();
-        this.done_at = null;
-        this.deleted_at = null;
-    }
+  changeStatus(statusAdd, statusRemove) {
+    this.status = statusAdd;
+    statusAdd === "delete"
+      ? (this.deleted_at = new Date())
+      : (this.done_at = new Date());
+    const element = document.querySelector("#" + this.id);
+    element.classList.add(statusAdd);
+    element.classList.remove(statusRemove);
+  }
 
-    done(){
-        this.status = 'done',
-        this.done_at = new Date();
-        const element = document.querySelector("#"+this.id);
-        element.classList.add('done');
-        element.classList.remove('todo');
-    }
+  done() {
+    this.changeStatus("done", "todo");
+  }
 
-    delete(){
-        this.status = 'delete',
-        this.deleted_at = new Date();
-        const element = document.querySelector("#"+this.id);
-        element.classList.add('delete');
-        element.classList.remove('todo');
-    }
+  delete() {
+    this.changeStatus("delete", "todo");
+  }
 
-    removeDone () {
-        this.status = 'todo';
-        const element = document.querySelector("#"+this.id);
-        element.classList.add('todo');
-        element.classList.remove('done');
-    }
+  update(newText) {
+    const element = document.querySelector(`#${this.id}`);
+    // children[0] es el primer hijo de la etiqueta <p>, el cual es <di>
+    // children[1] es el segundo hijo de la etiqueta <div<, el cual es <span>
+    element.children[0].children[1].textContent = newText;
+  }
 
-    createElement(){
-        const element = document.createElement('p');
-        element.setAttribute('id', this.id);
-        element.setAttribute('class','task todo');
-        element.innerHTML = `
+  removeDone() {
+    this.changeStatus("todo", "done");
+  }
+
+  createElement() {
+    const element = document.createElement("p");
+    element.setAttribute("id", this.id);
+    element.setAttribute("class", "task todo");
+    element.innerHTML = `
+        <div>
             <input type="checkbox" onchange="checkTask(this);">
             <span>${this.text}</span>
+        </div>
+        <div>
+            <a onclick="updateTask(this);">✍🏻</a>
             <a onclick="deleteTask(this);">❌</a>
+        </div>
         `;
-        return element;
-    }
-
+    return element;
+  }
 }
